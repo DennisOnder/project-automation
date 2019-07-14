68717733df70024476a4c9b3d8f23c572569e778
Denis Onder <webdev.dennisonder@gmail.com>
2019-07-15 00:07:06 +0200

    Language options

--- createProject.sh
+++ createProject.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-cd ~/Code
+printf 'Language (node, go): '
+read LANG
+
+case $LANG in
+  node)
+    echo "Creating a new Node project..."
+    cd ~/Code
+    ;;
+  go)
+    echo "Creating a new Golang project..."
+    cd ~/go/src/github.com/DennisOnder
+    ;;
+  *)
+    echo "Please provide a language from the list of supported options."
+    exit 1
+    ;;
+esac
+
 mkdir $1
 printf "Created folder %s \n" $1
 cd $1
